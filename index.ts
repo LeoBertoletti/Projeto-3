67340bd4883fa6d37335fc7fd6364224681b047a
LeoBertoletti <leonardo.bertoletti@gmail.com>
2023-05-15 20:25:27 -0300

    get e post funcionando

--- index.ts
+++ index.ts
@@ -1,12 +1,28 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
+import express from 'express'
+
+const app = express()
 const prisma = new PrismaClient()
+const port = 3000
+var contador = 0
 
-async function teste() {
-    await prisma.crews.create({
+app.get('/', async (req, res) => {
+    const crewList = await prisma.crews.findMany()
+    res.json(crewList)
+})
+
+app.post('/', async (req, res) => {
+    const post = await prisma.crews.create({
         data: {
-            ship: 'teste'
-        }
+            ship: `teste${contador}`
+            //author: { connect: { email: authorEmail } },
+        },
     })
-}
+    post
+    res.json('post')
+    contador++
+})
 
-teste()
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
